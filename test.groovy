80ad7844c11a7095a08de1f4e9a4f1de78e34925
Vlad Korolev <vlad@v-lad.org>
2019-01-05 09:25:38 -0800

    Test Job

--- test.groovy
+++ test.groovy
@@ -3,3 +3,24 @@ job('test-job') {
 
 
 }
+
+job("drain-job") {
+  label('cb2018')
+  
+  throttleConcurrentBuilds {
+      maxPerNode(1)
+      maxTotal(1)
+      categories(['CB',])
+  }
+
+  wrappers {
+      colorizeOutput()
+      timestamps()
+  }
+
+  publishers {
+    chucknorris()
+  }
+
+  steps { shell('sleep 10m') }
+}
