ab850c6a8cb0bb9432534eb61aca9fda17838776
Vlad Korolev <vlad@v-lad.org>
2019-01-05 20:42:09 -0800

    Setup a matrix job

--- test.groovy
+++ test.groovy
@@ -1,8 +1,3 @@
-job('test-job') {
-
-
-
-}
 
 job("drain-job") {
   label('cb2018')
@@ -24,3 +19,31 @@ job("drain-job") {
 
   steps { shell('docker run vladistan/cb2018t5:0.1 /p/find_ips 114 1') }
 }
+
+
+static String[] getSlices(n) {
+        return  (0..n).collect { "${it}" }
+}
+
+matrixJob("vlad-task-5") {
+
+     throttleConcurrentBuilds {
+            categories(['CB',])
+            throttleMatrixConfigurations(false)
+
+            maxPerNode(1)
+            maxTotal(1)
+    }
+  
+    axes {
+        label('node', ['cb2018',])
+        text('SLICE1', getSlices(2))
+        text('SLICE2', getSlices(3))
+    }
+  
+    steps {
+      shell('docker run vladistan/cb2018t5:0.1 /p/find_ips ${SLICE1} ${SLICE2}')
+    }
+   
+}
+
